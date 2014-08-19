cea8198627b940544e809d1d054900d9bd4eade8
alexei <hello@alexei.ro>
2014-08-19 13:12:56 +0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'backdrop',
+    packages = ['backdrop'],
+    version = '0.1',
+    description = 'Configuration system',
+    author = 'Alexei',
+    author_email = 'hello@alexei.ro',
+    url = 'https://github.com/alexei/backdrop',
+    keywords = ['configuration'],
+    classifiers = [],
+)
