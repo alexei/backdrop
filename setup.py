e46c9308bf91edd1fd494bb444b5d949c4dfbc0d
alexei <hello@alexei.ro>
2014-09-14 21:11:02 +0300

    setuptools is a pain in the ass

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from distutils.core import setup
 setup(
     name = 'backdrop',
     packages = ['backdrop'],
-    version = '0.1',
+    version = '0.0.1',
     description = 'Configuration system',
     author = 'Alexei',
     author_email = 'hello@alexei.ro',
     url = 'https://github.com/alexei/backdrop',
     download_url = 'https://github.com/alexei/backdrop/archive/0.0.1.tar.gz',
-    keywords = ['configuration'],
-    classifiers = [],
+    keywords = ['configuration']
 )
