d728590306085a53608427235cdde6f522d83e28
Alexei <hello@alexei.ro>
2015-09-17 18:07:40 +0300

    Adds option to specify config location

--- backdrop/__init__.py
+++ backdrop/__init__.py
@@ -2,11 +2,24 @@ import os
 import json
 
 class Backdrop(dict):
-    def __init__(self, project_name):
-        home_dir = os.path.expanduser('~')
-        file_name = "%s.json" % project_name
-        file_path = os.path.join(home_dir, '.backdrop', file_name)
-        file = open(file_path)
-        data = json.load(file)
-        file.close()
+    def __init__(self, project_name, directories=[]):
+        directories.reverse()
+
+        # first look into user's home directory
+        directories.append(os.path.expanduser('~'))
+
+        # then look into current directory
+        directories.append(os.path.dirname(os.path.realpath(__file__)))
+
+        directories.reverse()
+
+        data = {}
+        for directory in directories:
+            file_name = "%s.json" % project_name
+            file_path = os.path.join(directory, '.backdrop', file_name)
+            if os.path.isfile(file_path):
+                file = open(file_path)
+                data.update(json.load(file))
+                file.close()
+
         super(Backdrop, self).__init__(data)
